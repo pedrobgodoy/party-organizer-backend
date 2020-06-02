7e4dc9c27ff0347e2b6aded7ce9a9a022a938754
PedroBGodoy <pedrohenrique.godoy@outlook.com>
2020-06-01 23:08:25 -0300

    formmating code

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,106 +1,147 @@
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
 
 dotenv.config();
 
-const User = require('../models/User.js');
-const {parseHeaderAuthorization, validateEmail} = require('../utils/Utils.js');
+const User = require("../models/User.js");
+const {
+    parseHeaderAuthorization,
+    validateEmail,
+} = require("../utils/Utils.js");
 
 module.exports = {
-    async login(req, res){
+    async login(req, res) {
         let email = "";
         let password = "";
 
-        try{
+        try {
             [email, password] = parseHeaderAuthorization(req.headers);
-        } catch(e) {
-            return res.status(400).json({status: "error", message: "Dados inválidos!" });
+        } catch (e) {
+            return res
+                .status(400)
+                .json({ status: "error", message: "Dados inválidos!" });
         }
 
-        const emailLower = email.toLowerCase();
+        const user = await User.findOne({ email: email.toLowerCase }).select([
+            "password",
+            "name",
+            "email",
+            "location",
+        ]);
 
-        const user = await User.findOne({ email: emailLower }).select(['password', 'name', 'email', 'location']);
-        
-        if(!user){
-            return res.json({ status: "error", message: "Usuário não encontrado!" });
+        if (!user) {
+            return res.json({
+                status: "error",
+                message: "Usuário não encontrado!",
+            });
         }
 
         const validPassword = await bcrypt.compare(password, user.password);
 
-        if(!validPassword){
+        if (!validPassword) {
             return res.json({ status: "error", message: "Senha incorreta!" });
         }
 
-        const token = jwt.sign({name: user.name, email: user.email}, process.env.JWT_SECRET, {
-            expiresIn: '1 day'
-        });
+        const token = jwt.sign(
+            { name: user.name, email: user.email },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1 day",
+            }
+        );
 
         return res.json({
             status: "success",
             user: {
-                name: user.name, 
-                email: user.email, 
-                location: user.location
+                name: user.name,
+                email: user.email,
+                location: user.location,
             },
-            token
+            token,
         });
     },
-    async register(req, res){
+    async register(req, res) {
         const { name, email, password } = req.body;
 
-        if(!email){
-            return res.status(200).json({status: "error", message: "É necessário um email para prosseguir!"});
+        if (!email) {
+            return res.status(200).json({
+                status: "error",
+                message: "É necessário um email para prosseguir!",
+            });
         }
 
-        if(!password){
-            return res.status(200).json({status: "error", message: "É necessário uma senha para prosseguir!"});
+        if (!password) {
+            return res.status(200).json({
+                status: "error",
+                message: "É necessário uma senha para prosseguir!",
+            });
         }
 
-        if(!name){
-            return res.status(200).json({status: "error", message: "É necessário um nome para prosseguir!"});
+        if (!name) {
+            return res.status(200).json({
+                status: "error",
+                message: "É necessário um nome para prosseguir!",
+            });
         }
 
         const emailLower = email.toLowerCase();
         const nameFormated = name.trim();
 
-        if(!validateEmail(emailLower)){
-            return res.status(200).json({status: "error", message: "Email inválido!"});
+        if (!validateEmail(emailLower)) {
+            return res
+                .status(200)
+                .json({ status: "error", message: "Email inválido!" });
         }
 
-        let user = await User.findOne({email: emailLower});
+        let user = await User.findOne({ email: emailLower });
 
-        if(user){
-            return res.status(200).json({status: "error", message: "Usuário já existe!"});
+        if (user) {
+            return res
+                .status(200)
+                .json({ status: "error", message: "Usuário já existe!" });
         }
 
-        if(password.length <= 3){
-            return res.status(200).json({status: "error", message: "A senha deve conter mais de 3!"});
+        if (password.length <= 3) {
+            return res.status(200).json({
+                status: "error",
+                message: "A senha deve conter mais de 3!",
+            });
         }
 
-        const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS)).catch(err => console.log(err));
-        const hashedPassword = await bcrypt.hash(password, salt).catch(err => console.log(err));
+        const salt = await bcrypt
+            .genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS))
+            .catch((err) => console.log(err));
+        const hashedPassword = await bcrypt
+            .hash(password, salt)
+            .catch((err) => console.log(err));
 
         user = await User.create({
             name: nameFormated,
             email: emailLower,
-            password: hashedPassword
-        })
+            password: hashedPassword,
+        });
 
-        if(!user){
-            return res.status(200).json({status: "error", message: "Erro ao criar usuário!"});    
+        if (!user) {
+            return res
+                .status(200)
+                .json({ status: "error", message: "Erro ao criar usuário!" });
         }
 
-        const token = jwt.sign({name: user.name, email: user.email}, process.env.JWT_SECRET, {
-            expiresIn: '1 day'
-        });
+        const token = jwt.sign(
+            { name: user.name, email: user.email },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1 day",
+            }
+        );
 
         const returnUser = {
             name: user.name,
             email: user.email,
-            location: user.location
-        }
+            location: user.location,
+        };
 
-        return res.status(200).json({status: "success", returnUser, token});
-    }
-}
+        return res.status(200).json({ status: "success", returnUser, token });
+    },
+};
